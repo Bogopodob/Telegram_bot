f33f80d4175df9d654a06f522576ac26ae155de3
bogopodob <bogopodob@gmail.com>
2020-04-10 02:19:48 +0700

    Moved to folder "Supporting".

--- app/Classes/Supporting/Date.php
+++ app/Classes/Supporting/Date.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace App\Classes;
-
+namespace App\Classes\Supporting;
 
 class Date {
 
@@ -47,11 +46,11 @@ class Date {
 	 * @return string
 	 */
 	public function fullDataFormat (string $date) : string {
-		$dateYear = date('Y', strtotime($date));
-		$dateDay = date('d', strtotime($date));
-		$dateWeek = (int)date('w', strtotime($date));
+		$dateYear  = date('Y', strtotime($date));
+		$dateDay   = date('d', strtotime($date));
+		$dateWeek  = (int)date('w', strtotime($date));
 		$dateMonth = (int)date('m', strtotime($date));
-		$dateTime = date('H:i', strtotime($date));
+		$dateTime  = date('H:i', strtotime($date));
 		return "{$this->dayWeek(($dateWeek === 0) ?: $dateWeek - 1)}, $dateDay {$this->month(($dateMonth === 0) ?: $dateMonth - 1)} $dateYear г, в $dateTime";
 	}
 }
