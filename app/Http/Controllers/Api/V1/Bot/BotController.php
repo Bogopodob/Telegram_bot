812b688e99706880359ffb5a51fe9ec53142f235
bogopodob <bogopodob@gmail.com>
2020-04-14 21:09:07 +0700

    User id changed in test data.

--- app/Http/Controllers/Api/V1/Bot/BotController.php
+++ app/Http/Controllers/Api/V1/Bot/BotController.php
@@ -18,7 +18,7 @@ class BotController extends Controller {
 		// @tomsk_innovation_test_bot - название бота
 		// Примеры ответов от телеграмма
 		// ** json когда пользователь вводит команду
-		//    {"update_id":788689423,"message":{"message_id":26,"from":{"id":464546055,"is_bot":false,"first_name":"\u0415\u0432\u0433\u0435\u043d\u0438\u0439","last_name":"\u0411\u0430\u043b\u0438\u043a\u0430","username":"bogopodob","language_code":"ru"},"chat":{"id":-1001402617953,"title":"bogopodob_test","type":"supergroup"},"date":1585957266,"text":"\/last 3","entities":[{"offset":0,"length":5,"type":"bot_command"}]}}
+		//    {"update_id":788689423,"message":{"message_id":26,"from":{"id":464546055,"is_bot":false,"first_name":"\u0415\u0432\u0433\u0435\u043d\u0438\u0439","last_name":"\u0411\u0430\u043b\u0438\u043a\u0430","username":"bogopodob","language_code":"ru"},"chat":{"id":464546055,"title":"bogopodob_test","type":"supergroup"},"date":1585957266,"text":"\/last 3","entities":[{"offset":0,"length":5,"type":"bot_command"}]}}
 		// *** Когда пользователь пишет сообщения
 		//    {"update_id":788689258,"message":{"message_id":174,"from":{"id":464546055,"is_bot":false,"first_name":"\u0415\u0432\u0433\u0435\u043d\u0438\u0439","last_name":"\u0411\u0430\u043b\u0438\u043a\u0430","username":"bogopodob","language_code":"ru"},"chat":{"id":464546055,"first_name":"\u0415\u0432\u0433\u0435\u043d\u0438\u0439","last_name":"\u0411\u0430\u043b\u0438\u043a\u0430","username":"bogopodob","type":"private"},"date":1585854250,"text":"\u041f\u0440\u0438\u0432\u0435\u0442"}}
 
