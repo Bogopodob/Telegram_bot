b1977ad759184902473b9d563a8e346dfd2f818a
bogopodob <bogopodob@gmail.com>
2020-04-16 15:16:27 +0700

    $request->post() has been added to file_get_contents.

--- app/Http/Controllers/Api/V1/Bot/BotController.php
+++ app/Http/Controllers/Api/V1/Bot/BotController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Api\V1\Bot;
 
 use App\Classes\RequestBot;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class BotController extends Controller {
-	public function bot () {
+	public function bot (Request $request) {
 
 		// ********************************
 		// Перед запуском, в директории
@@ -22,8 +23,10 @@ class BotController extends Controller {
 		// *** Когда пользователь пишет сообщения
 		//    {"update_id":788689258,"message":{"message_id":174,"from":{"id":464546055,"is_bot":false,"first_name":"\u0415\u0432\u0433\u0435\u043d\u0438\u0439","last_name":"\u0411\u0430\u043b\u0438\u043a\u0430","username":"bogopodob","language_code":"ru"},"chat":{"id":464546055,"first_name":"\u0415\u0432\u0433\u0435\u043d\u0438\u0439","last_name":"\u0411\u0430\u043b\u0438\u043a\u0430","username":"bogopodob","type":"private"},"date":1585854250,"text":"\u041f\u0440\u0438\u0432\u0435\u0442"}}
 
-		$data = file_get_contents('php://input');
-		$data = json_decode($data, TRUE);
+		$data = $request->post();
+
+		if (!$data)
+			return json_encode(NULL);
 
 		$Bot = new RequestBot();
 		return $Bot->request($data);
